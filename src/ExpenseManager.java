3ffb21f24ed90cee1fa19d7156e55a1b4ee75eb3
Noé <rodrigueznue9@gmail.com>
2024-04-26 16:55:34 +0200

    Se han añadido métodos para alistar y resumir gastos

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -1,5 +1,34 @@
 // This class manages a list of Expense objects and includes methods to add,
 // list, and summarize expenses, as well as to validate inputs and handle file data.
-public class ExpenseManager {
-    
+import java.io.;
+import java.util.;
+
+public class ExpenseManager extends Expense {
+    private List<Expense> expenses;
+
+
+    public ExpenseManager(double amount, String category) {
+        super(amount, category);
+        this.expenses = new ArrayList<>(); {
+        this.expenses = new ArrayList<>();
+    }
+}
+
+    public void addExpense(Expense expense) {
+        expenses.add(expense);
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public double summarizeExpensesByCategory(String category) {
+        double total = 0;
+        for (Expense expense : expenses) {
+            if (expense.getCategory().equals(category)) {
+                total += expense.getAmount();
+            }
+        }
+        return total;
+    }
 }
